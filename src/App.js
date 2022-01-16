36dc359f8aa369ef3df9a49e1a7e59bd764aa08b
Mikhail1JS <misha.zaichko@gmail.com>
2022-01-16 12:10:58 +0500

    changed files

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import './App.scss';
 
 function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        My first React App
-        <h3>Hello, {props.name}</h3>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          My first React App
+          <h3>Hello, {props.name}</h3>
+        </header>
+      </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -8,20 +8,19 @@ import './giphy-700.gif';
 const myName = 'Mikhail';
 const bodyName = 'main__wrap';
 const somMessage = 'Look! It\'s moving. It\'s alive. It\'s alive... It\'s alive, it\'s moving, it\'s alive, it\'s alive, it\'s alive, it\'s alive, IT\'S ALIVE!';
-const imgFrank = 'myapp/public/giphy-700.gif';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App name = {myName} />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App name = {myName} />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 ReactDOM.render(
     <React.StrictMode>
-        <Message name = {bodyName} text = {somMessage} image={imgFrank} />
+        <Message name = {bodyName} text = {somMessage}  />
     </React.StrictMode>,
     document.getElementById('second-block')
 );
-
 
