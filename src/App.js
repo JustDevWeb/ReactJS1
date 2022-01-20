a9c40541b39d2448cd82aeb6660f8007fd9a2908
Mikhail1JS <misha.zaichko@gmail.com>
2022-01-20 17:35:09 +0500

    home work 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,123 @@
 import './App.scss';
+import React, {useEffect, useState} from "react";
 
-function App(props) {
-  return (
-      <div className="App">
-        <header className="App-header">
-          My first React App
-          <h3>Hello, {props.name}</h3>
-        </header>
-      </div>
-  );
+
+
+function App() {
+
+    const [messageList, setMessageList] = useState([]);
+    const [isLogin, setIsLogin] = useState(false);
+    const [login, setLogin] = useState("");
+    const [chatMessage, setChatMessage] = useState("");
+
+
+
+
+    const updateLogin = (e) => {
+        setLogin(e.target.value)
+    }
+    const updateChatMessage = (e) => {
+        setChatMessage(e.target.value);
+    }
+    const updateMessageList = (e) => {
+
+        setMessageList(prevMessageList => (prevMessageList.concat(
+            {author: login, text: chatMessage}))
+        );
+        setChatMessage("");
+    };
+
+
+    const toggleForm = (e) => {
+        setIsLogin(!isLogin);
+    };
+
+    useEffect(() => {
+        let interval=0;
+        if (messageList.length>0) {
+            if (messageList[messageList.length - 1].author != "robot") {
+
+                interval = setInterval(() => {
+                    setMessageList(prevMessageList =>
+                        prevMessageList.concat({author: "robot", text: "this message was generated automatically"}))
+
+                }, 1500)
+            }
+        }
+    return ()=>{
+            clearInterval(interval)
+    }
+    }, [messageList])
+
+
+    return (
+        <div>
+            <div className={'chat-wrapper'}>
+                <h1 className={'form-title'}>Messenger</h1>
+                {!isLogin ? (<form className={"login-form"}>
+                    <input required={true} className={"login-form__field"} value={login} onChange={updateLogin}
+                           placeholder={"Please enter your user name"}/>
+                    <button className={"login-form__button button"} type={'submit'} onClick={toggleForm}>Confirm</button>
+                </form>) : (<form className={"my-form"}>
+                    <textarea required={true} onChange={updateChatMessage}
+                              placeholder={" Write something here , please!"} className={"textarea"} value={chatMessage}
+                              id="text-here" cols="30" rows="5">
+                    </textarea>
+                    <button disabled={!chatMessage} onClick={updateMessageList} className={'form-submit button'}
+                            type={"button"}> Ready to send
+                    </button>
+                </form>)}
+                <div className={"chat-box"}>
+                    {messageList.map(message => <div className={"messageBox"}>
+                        <span>{message.author}</span>
+                        <p>{message.text}</p>
+                    </div>)}
+                </div>
+
+            </div>
+
+
+        </div>
+    )
 }
 
+// class App extends React.Component{
+//     state = {
+//         massageList : [
+//             {
+//                 author: 'Louis L’Amour',
+//                 text: 'Start writing, no matter what. The water does not flow until the faucet is turned on.'
+//         },
+//             {
+//                 author: 'Steven Pressfield',
+//                 text: 'Start before you’re ready.'
+//             },
+//             {
+//                 author: 'Jodi Picoult',
+//                 text: 'You can always edit a bad page. You can’t edit a blank page'
+//             },
+//             {
+//                 author: 'Ray Bradbury',
+//                 text: 'First, find out what your hero wants, then just follow him!'
+//             }
+//         ]
+//     }
+//
+//     render() {
+//         return (
+//                 <div>
+//                     <div className={'quotes-block'}>
+//                     <h1 className={'quotes-title'}>Always be codding</h1>
+//                     {this.state.massageList.map(message=><div className={'quotes'}>
+//                     <blockquote><p>{message.text}</p><cite>{message.author}</cite></blockquote>
+//
+//                 </div>)}</div>
+
+
+//                 </div>
+//         )
+//     }
+// }
+//
 export default App;
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Message from  './hw1';
-import './giphy-700.gif';
+
 
 const myName = 'Mikhail';
 const bodyName = 'main__wrap';
@@ -13,7 +13,7 @@ const imgFrank = 'myapp/public/giphy-700.gif';
 ReactDOM.render(
     <React.StrictMode>
         <App name = {myName} />
-        <Message name = {bodyName} text = {somMessage} image={imgFrank} />
+        {/*<Message name = {bodyName} text = {somMessage} image={imgFrank} />*/}
     </React.StrictMode>,
     document.getElementById('root')
 );
