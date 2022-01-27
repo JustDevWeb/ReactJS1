169b45e15614c6e9c67858eaf76ed7c2a572e532
Mikhail1JS <misha.zaichko@gmail.com>
2022-01-27 18:07:42 +0500

    homework3

--- src/App.js
+++ src/App.js
@@ -1,40 +1,49 @@
 import './App.scss';
 import React, {useEffect, useState , useRef} from "react";
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+
 
 
 
 function App() {
 
     const [messageList, setMessageList] = useState([]);
-    const [isLogin, setIsLogin] = useState(false);
-    const [login, setLogin] = useState("");
+    const [login] = useState({me:"me",bot:"bot"});
     const [chatMessage, setChatMessage] = useState("");
+    const [chats] = useState([{id:1, name: 'Alex' },{id:2, name: 'Bryan'}, {id:3, name: 'Kate'}])
 
     const time= new Date();
     const currentTime= `${time.getHours()}:${time.getMinutes()}`
     const messagesEndRef = useRef(null)
+    const inputFocus =useRef(null)
 
 
 
 
-        const updateLogin = (e) => {
-        setLogin(e.target.value)
-    }
+
     const updateChatMessage = (e) => {
         setChatMessage(e.target.value);
     }
     const updateMessageList = (e) => {
 
         setMessageList(prevMessageList => (prevMessageList.concat(
-            {author: login, text: chatMessage, time:currentTime}))
+            {author: login.me, text: chatMessage, time:currentTime}))
         );
         setChatMessage("");
     };
 
 
-    const toggleForm = (e) => {
-        setIsLogin(!isLogin);
-    };
+    // const toggleForm = (e) => {
+    //     setIsLogin(!isLogin);
+    // };
 
     useEffect(() => {
         let interval=0;
@@ -43,7 +52,7 @@ function App() {
 
                 interval = setInterval(() => {
                     setMessageList(prevMessageList =>
-                        prevMessageList.concat({author: "bot", text: "this message was generated automatically",time:currentTime}))
+                        prevMessageList.concat({author: login.bot, text: "this message was generated automatically",time:currentTime}))
 
                 }, 1500)
             }
@@ -57,25 +66,82 @@ function App() {
     //Auto scrolling
     useEffect(() => {
         messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+        inputFocus.current.focus();
     });
 
     return (
-        <div>
-            <div className={'chat-wrapper'}>
+        <div className={'App'}>
+                <div className={'chats-box'}>
+                    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                        <ListItem alignItems="flex-start">
+                            <ListItemAvatar>
+                                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                            </ListItemAvatar>
+                            <ListItemText
+                                primary={chats[0].name}
+                                secondary={
+                                    <React.Fragment>
+                                        <Typography
+                                            sx={{ display: 'inline' }}
+                                            component="span"
+                                            variant="body2"
+                                            color="text.primary"
+                                        >
+                                            Ali Connors
+                                        </Typography>
+                                        {" — I'll be in your neighborhood doing errands this…"}
+                                    </React.Fragment>
+                                }
+                            />
+                        </ListItem>
+                        <Divider variant="inset" component="li" />
+                        <ListItem alignItems="flex-start">
+                            <ListItemAvatar>
+                                <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+                            </ListItemAvatar>
+                            <ListItemText
+                                primary={chats[1].name}
+                                secondary={
+                                    <React.Fragment>
+                                        <Typography
+                                            sx={{ display: 'inline' }}
+                                            component="span"
+                                            variant="body2"
+                                            color="text.primary"
+                                        >
+                                            to Scott, Alex, Jennifer
+                                        </Typography>
+                                        {" — Wish I could come, but I'm out of town this…"}
+                                    </React.Fragment>
+                                }
+                            />
+                        </ListItem>
+                        <Divider variant="inset" component="li" />
+                        <ListItem alignItems="flex-start">
+                            <ListItemAvatar>
+                                <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+                            </ListItemAvatar>
+                            <ListItemText
+                                primary={chats[2].name}
+                                secondary={
+                                    <React.Fragment>
+                                        <Typography
+                                            sx={{ display: 'inline' }}
+                                            component="span"
+                                            variant="body2"
+                                            color="text.primary"
+                                        >
+                                            Sandra Adams
+                                        </Typography>
+                                        {' — Do you have Paris recommendations? Have you ever…'}
+                                    </React.Fragment>
+                                }
+                            />
+                        </ListItem>
+                    </List>
+                </div>
+                <div className={'chat-wrapper'}>
                 <h1 className={'form-title'}>Messenger</h1>
-                {!isLogin ? (<form className={"login-form"}>
-                    <input required={true} className={"login-form__field"} value={login} onChange={updateLogin}
-                           placeholder={"Please enter your user name"}/>
-                    <button className={"login-form__button button"} type={'submit'} onClick={toggleForm}>Confirm</button>
-                </form>) : (<form className={"my-form"}>
-                    <textarea required={true} onChange={updateChatMessage}
-                              placeholder={" Write something here , please!"} className={"textarea"} value={chatMessage}
-                              id="text-here" cols="30" rows="5">
-                    </textarea>
-                    <button disabled={!chatMessage} onClick={updateMessageList} className={'form-submit button'}
-                            type={"button"}> Ready to send
-                    </button>
-                </form>)}
                 <div className={"chat-box"}>
                     {messageList.map((message,index) => <div className={`messageBox ${message.author==='bot' ? 'bot': 'user'}`} key={index}>
                         <span>{message.author}</span>
@@ -83,51 +149,20 @@ function App() {
                     </div>)}
                     <div ref={messagesEndRef}/>
                 </div>
-
-            </div>
-
-
+                <form className={"my-form"}>
+                    <input ref={inputFocus} required={true} onChange={updateChatMessage} autoFocus={true}
+                              placeholder={" Write something here , please!"} className={"textarea"} value={chatMessage}
+                              id="text-here" >
+                    </input>
+                    <Button  className={'send-button'} disabled={!chatMessage} onClick={updateMessageList} variant="contained" endIcon={<SendIcon />}>
+                        Send
+                    </Button>
+                </form>
+                </div>
         </div>
     )
 }
 
-// class App extends React.Component{
-//     state = {
-//         massageList : [
-//             {
-//                 author: 'Louis L’Amour',
-//                 text: 'Start writing, no matter what. The water does not flow until the faucet is turned on.'
-//         },
-//             {
-//                 author: 'Steven Pressfield',
-//                 text: 'Start before you’re ready.'
-//             },
-//             {
-//                 author: 'Jodi Picoult',
-//                 text: 'You can always edit a bad page. You can’t edit a blank page'
-//             },
-//             {
-//                 author: 'Ray Bradbury',
-//                 text: 'First, find out what your hero wants, then just follow him!'
-//             }
-//         ]
-//     }
-//
-//     render() {
-//         return (
-//                 <div>
-//                     <div className={'quotes-block'}>
-//                     <h1 className={'quotes-title'}>Always be codding</h1>
-//                     {this.state.massageList.map(message=><div className={'quotes'}>
-//                     <blockquote><p>{message.text}</p><cite>{message.author}</cite></blockquote>
-//
-//                 </div>)}</div>
 
-
-//                 </div>
-//         )
-//     }
-// }
-//
 export default App;
 
--- src/index.js
+++ src/index.js
@@ -2,18 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Message from  './hw1';
+
 
 
 const myName = 'Mikhail';
-const bodyName = 'main__wrap';
-const somMessage = 'Look! It\'s moving. It\'s alive. It\'s alive... It\'s alive, it\'s moving, it\'s alive, it\'s alive, it\'s alive, it\'s alive, IT\'S ALIVE!';
-const imgFrank = 'myapp/public/giphy-700.gif';
+
 
 ReactDOM.render(
     <React.StrictMode>
-        <App name = {myName} />
-        {/*<Message name = {bodyName} text = {somMessage} image={imgFrank} />*/}
+        <App />
     </React.StrictMode>,
     document.getElementById('root')
 );
